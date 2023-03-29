47d69bc320eed71f5d68531b7e4e7c43c4e0a5c6
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-03-29 00:59:25 -0500

    Added questions for my prompt array

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title", 
+        message:"What is your project title?", 
+        type: "input"
+    },
+    {
+        name: 'description', 
+        message: "What's the project description?", 
+        type: 'input'
+    },
+    {
+        name: 'install', 
+        message: "What are the installation instructions?", 
+        type: 'input'
+    },
+    {
+        name: 'usage', 
+        message:"What is the usage information?", 
+        type: 'input'
+    },
+    {
+        name: 'guidelines',
+        message: "What are the contribution guidelines?",
+        type: 'input'
+    },
+    {
+        name: 'test', 
+        message: "What are the test instructions?", 
+        type: 'input'
+    },
+    {
+        name: 'license', 
+        message: "What license is your project using?", 
+        type: 'list', 
+        choices:['MIT', 'GNU']
+    },
+    {
+        name: 'user', 
+        message: "What is your GitHub username?", 
+        type: 'input'
+    },
+    {
+        name: 'email', 
+        message: "What is your GitHub email?", 
+        type: 'input'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
